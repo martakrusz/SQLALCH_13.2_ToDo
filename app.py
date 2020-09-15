a2d7ae82ff85c329bbadbcd66940610df7059077
Marta Kruszewska <martakruszewska007@gmail.com>
2020-09-15 20:21:33 +0200

    valueerror

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import (
+    Flask, 
+    render_template, 
+    request, 
+    redirect, 
+    url_for,
+)
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
 app = Flask(__name__)
-"""
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-"""
+
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-    description = db.Column(db.String(100))
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(100), nullable=False)
     complete = db.Column(db.Boolean)
 
 @app.route('/')
@@ -26,12 +28,15 @@ def index():
 @app.route('/add', methods =["POST"] )
 def add():
     #add new item
-    title = request.form.get("title")
-    description = request.form.get("description")
-    new_todo = Todo(title=title, description=description, complete=False)
-    db.session.add(new_todo)
-    db.session.commit()
-    return redirect(url_for("index"))
+    try:
+        title = request.form.get("title")
+        description = request.form.get("description")
+        new_todo = Todo(title=title, description=description, complete=False)
+        db.session.add(new_todo)
+        db.session.commit()
+        return redirect(url_for("index"))
+    except ValueError as error:
+        return render_template("base.html", message=str(error))
 
 @app.route('/update/<int:todo_id>')
 def update(todo_id):
@@ -48,7 +53,6 @@ def delete(todo_id):
     db.session.delete(todo)
     db.session.commit()
     return redirect(url_for("index"))
-
+db.create_all()
 if __name__=="__main__":
-    db.create_all()
     app.run(debug=True)
